de11a0f3eaee7e051b8a52a4bcd0f52e49c0832d
KiryuKazuha <suyuze2014@gmail.com>
2023-06-08 23:41:06 +0800

    Customized Premake

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Nova"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Nova"
+	location "Nova"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/3rdParty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NV_PLATFORM_WINDOWS",
+			"NV_BUILD_DLL",
+			"_WINDLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+	filter "configurations:debug"
+		defines "NV_DEBUG"
+		symbols "On"
+
+	filter "configurations:releaase"
+		defines "NV_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "NV_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Nova/3rdParty/spdlog/include",
+		"Nova/src"
+	}
+
+	links
+	{
+		"Nova"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NV_PLATFORM_WINDOWS"
+		}
+	
+	filter "configurations:debug"
+		defines "NV_DEBUG"
+		symbols "On"
+
+	filter "configurations:releaase"
+		defines "NV_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "NV_DIST"
+		optimize "On"
